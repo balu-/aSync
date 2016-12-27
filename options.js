eb2e4151438ddc903064ed35dba83b9a9ca417e6
balu <balu@example.com>
2016-12-27 01:45:04 +0100

    don't ask

--- options.js
+++ options.js
@@ -1,11 +1,3 @@
-function saveOptions(e) {
-  e.preventDefault();
-  browser.storage.local.set({
-    ip: document.querySelector("#ip").value
-  });
-}
-
-
 //enable / disable inputelements in the valueArea
 function changeForm(enable){
   var ar = document.querySelectorAll("#valueArea input");
@@ -16,7 +8,27 @@ function changeForm(enable){
   }
 }
 
-function enabledChanged(e) {
+//save form settings to local storage
+function saveForm(){
+  var ar = document.querySelectorAll("input");
+  if(ar != null){
+    ar.forEach(function(element) {
+      var stObj = {};
+      
+        if(element.type == "checkbox"){
+           stObj["object_"+element.id] = element.checked;
+        } else if(element.type == "text"){
+          stObj["object_"+element.id] = element.value;
+        } 
+        console.log("Save: " + element.id +" - Value - " +  stObj["object_"+element.id]);
+        browser.storage.local.set(stObj);
+      
+    });
+  }
+}
+
+
+function enabledChanged() {
   if(document.querySelector("#isEnabled").checked){
     console.log("enabled");
     changeForm(false);
@@ -29,20 +41,50 @@ function enabledChanged(e) {
 
 function restoreOptions() {
 
-  function setCurrentChoice(result) {
-    document.querySelector("#ip").value = result.color || "0.0.0.0";
-  }
-
-  function onError(error) {
-    console.log(`Error: ${error}`);
-  }
-
-  //var getting = browser.storage.local.get("ip");
-  //getting.then(setCurrentChoice, onError);
-
   document.querySelector("#isEnabled").addEventListener("change", enabledChanged);
+
+  var ar = document.querySelectorAll("input");
+  if(ar != null){
+    ar.forEach(function(element) {
+      element.addEventListener("change", saveForm);
+    });
+  }
+
+  //load values
+  var ar = document.querySelectorAll("input");
+  if(ar != null){
+    ar.forEach(function(element) {
+      console.log("ID:" + element.id);
+      var promis = browser.storage.local.get("object_"+element.id);
+
+      promis.then(function(settingObj){
+        //got
+        console.log("Got Settings " + JSON.stringify(settingObj));
+
+        var fieldValue = settingObj['object_'+element.id];
+        if(fieldValue != null){  
+
+          if(element.type == "checkbox"){
+            if(fieldValue){ // fieldValue = true
+              element.checked = true;
+            } 
+          } else if(element.type == "text"){
+            element.value = fieldValue;
+          } 
+            console.log("Loaded value " + fieldValue );
+        }
+
+        enabledChanged(); // refresh enabled status
+
+      }, function(err) {
+        // body...
+        console.err("Could not get Setting " + element.id);
+      });
+
+    });
+  }
 }
 
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+//document.querySelector("form").addEventListener("submit", saveOptions);
